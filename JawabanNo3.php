cd5ce86ca0f14f31ee61aa2ff9fb7791b1d3da7a
shoifuddin <52595830+shoifuddin@users.noreply.github.com>
2019-07-06 21:09:49 +0700

    Add files via upload

---
+++ JawabanNo3.php
@@ -0,0 +1,44 @@
+<?php
+$inputBilangan =10;
+
+function operasi($a){
+	$bilangan=12;
+	$batas=$bilangan;
+	$i=0;
+	if($bilangan%2==0)
+	{
+		while($a!=0)
+		{
+			if($batas==1)
+			{
+				$batas=0;
+			}
+
+			$batas=$batas/2;
+
+		}
+
+
+	}
+}
+
+
+	$bilangan=12;
+	$batas=$bilangan;
+	$i=0;
+	if($bilangan%2==0)
+	{
+		while($a!=0)
+		{
+			if($batas==1)
+			{
+				$batas=0;
+			}
+
+			$batas=$batas/2;
+			$i++;
+		}
+		echo $i;
+
+	}
+?>
